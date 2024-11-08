5fd41cfe7dc58f052bc2ed3ddd547b5edba79ee5
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-11-09 02:05:49 +0530

    Deployment bug fix

--- app/component/GetStartedSection.jsx
+++ app/component/GetStartedSection.jsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import React from "react";
 import a from "../../public/images/stockPictures/howToGetStarted/1.png";
--- app/component/HeroSection.jsx
+++ app/component/HeroSection.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import Image from "next/image";
 import heroImage from "../../public/images/stockPictures/HeroSection/iamge.png"; // Renamed for clarity
